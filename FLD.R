a2ed6f0215683367123eb24ed22ccbc11f9073b2
JayRendo <97909928+JayRendo@users.noreply.github.com>
2022-02-25 14:07:23 +0100

    Create FLD.R

---
+++ FLD.R
@@ -0,0 +1,28 @@
+#For reproducibility
+set.seed(1)
+
+#Loading the data and the necessary libraries
+data <- iris[1:100,]
+data$Species <- as.character(data$Species)
+data$Species <- as.factor(data$Species)
+library(MASS)
+
+#Dividing it into a train and test set
+sample <- sample(seq(1,2), size = nrow(data), replace = TRUE, prob = c(0.8,0.2))
+data_train <- data[sample == 1, ]
+data_test <- data[sample == 2, ]
+
+#Training the method
+model <- lda(Species ~., data = data_train)
+
+#Figuring out the accuracy on the test model
+#The test set is fairly balanced so this is an 
+#accurate measure
+y_pred <- predict(model, newdata = data_test[,1:4])$class
+count <- 0
+for (i in 1:nrow(data_test)){
+  if (y_pred[i] == data_test$Species[i]){
+    count = count + 1
+  }
+}
+count/nrow(data_test)
