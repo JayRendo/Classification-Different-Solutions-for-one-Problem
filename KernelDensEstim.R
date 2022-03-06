69f29d9291f1c8aa6764eb6b38d1b86f2189bd71
JayRendo <97909928+JayRendo@users.noreply.github.com>
2022-03-06 16:02:15 +0100

    Create KernelDensEstim.R

---
+++ KernelDensEstim.R
@@ -0,0 +1,21 @@
+##STILL NEED TO FINISH THIS
+#For reproducibility
+set.seed(1)
+#Importing the data
+library(PPCI)
+data <- optidigits
+data <- data[c(1,2)]
+data <- data.frame(data)
+data$c <- as.factor(data$c)
+#Dividing data into train and test set
+sample <- sample(seq(1,2), size = nrow(data), replace = TRUE, prob = c(0.9,0.1))
+data_train <- data[sample == 1, ]
+data_test <- data[sample == 2, ]
+
+#Computing the densities and using them for classification
+library(np)
+list_dens = rep(NA, 10)
+for (i in 1:10){
+  bw <- npudensbw(tdat = data_train[which(data_train$c == i-1), 1:64])
+  list_dens[i] <- npudens(bw)
+}
