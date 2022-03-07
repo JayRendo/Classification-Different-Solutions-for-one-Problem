becab7355594e3e88ceb302d507d6ce6b5e6a236
JayRendo <97909928+JayRendo@users.noreply.github.com>
2022-03-07 15:25:45 +0100

    Update kNN.R

--- kNN.R
+++ kNN.R
@@ -9,24 +9,18 @@ sample <- sample(seq(1,2), size = nrow(data), replace = TRUE, prob = c(0.8,0.2))
 data_train <- data[sample == 1, ]
 data_test <- data[sample == 2, ]
 
-#testing the performance of the algorithm
+#testing the performance of the algorithm, depnding on the value of k
 library(class)
-#Here k = 1
-y_pred <- knn(data_train[,c(2,3)], data_test[,c(2,3)], data_train$Sex)
-count <- 0
-for (i in 1:nrow(data_test)) {
-  if(y_pred[i] == data_test$Sex[i]){
-    count <- count + 1
+classif_rate <- rep(NA, 121)
+for(k in 1:121){ #above 121 we get warnings
+  y_pred <- knn(data_train[,c(2,3)],  data_test[,c(2,3)], data_train$Sex, k = k)
+  count <- 0
+  for (i in 1:nrow(data_test)) {
+    if(y_pred[i] == data_test$Sex[i]){
+      count <- count + 1
+    }
   }
+  classif_rate[k] <-  (count/nrow(data_test))
 }
-count/nrow(data_test)
-
-#Here k corresponds to using half of the closest data
-y_pred <- knn(data_train[,c(2,3)], data_test[,c(2,3)], data_train$Sex, k = 72)
-count <- 0
-for (i in 1:nrow(data_test)) {
-  if(y_pred[i] == data_test$Sex[i]){
-    count <- count + 1
-  }
-}
-count/nrow(data_test)
+plot(classif_rate, xlab = "k", ylab ="Classification accuracy", type = "l")
+which.max(classif_rate)
