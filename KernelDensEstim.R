481aeb757c1d61c08967a3b9a23b443e6c5be7d9
JayRendo <97909928+JayRendo@users.noreply.github.com>
2022-03-29 10:25:30 +0200

    Update KernelDensEstim.R

--- KernelDensEstim.R
+++ KernelDensEstim.R
@@ -1,23 +1,47 @@
-##STILL NEED TO FINISH THIS
+
 #For reproducibility
 set.seed(1)
 #Importing the data
-library(PPCI)
-data <- optidigits
-data <- data[c(1,2)]
-data <- data.frame(data)
-data$c <- as.factor(data$c)
+
+data <- iris
+data <- data[,c(1,5)] #only keep one attribute for simplicity
 #Dividing data into train and test set
-sample <- sample(seq(1,2), size = nrow(data), replace = TRUE, prob = c(0.9,0.1))
+sample <- sample(seq(1,2), size = nrow(data), replace = TRUE, prob = c(0.7,0.3))
 data_train <- data[sample == 1, ]
 data_test <- data[sample == 2, ]
 
-#Computing the densities and using them for classification
-library(np)
-library(MASS)
-list_dens = rep(NA, 10)
-for (i in 1:10){
-  bw <- bandwidth.nrd(data_train[which(data_train$c == i-1),1:64])
-  list_dens[i] <- npudens(bw)
-}
+acc <- rep(-1,100)
+bwvec <- seq(from = 0.01, to = 1, by = 0.01)
+for(j in 1:100){
 
+#Computing the densities and using them for classification
+denshat1 <- density(data[which(data$Species == "setosa"),1], bw = bwvec[j])
+densfct1 <- approxfun(denshat1$x,denshat1$y, rule = 2)
+denshat2 <- density(data[which(data$Species == "versicolor"),1], bw = bwvec[j])
+densfct2 <- approxfun(denshat2$x, denshat2$y, rule = 2)
+denshat3 <- density(data[which(data$Species == "virginica"),1], bw = bwvec[j])
+densfct3 <- approxfun(denshat3$x,denshat3$y, rule = 2)
+
+#Computing the predicted value and accuracy
+count <- 0
+for(i in 1:length(data_test[,1]) ){
+  y1 <- densfct1(data_test[i,1])
+  y2 <- densfct2(data_test[i,1])
+  y3 <- densfct3(data_test[i,1])
+  if(y1 == max(c(y1,y2,y3))){
+    if(data_test[i,2] == "setosa"){
+      count <- count + 1
+    }
+  }else if(y2 == max(c(y1,y2,y3))){
+    if(data_test[i,2] == "versicolor"){
+      count <- count + 1
+    }
+  }else if(y3 == max(c(y1,y2,y3))){
+    if(data_test[i,2] == "virginica"){
+      count <- count + 1
+    }
+  }
+}
+acc[j] <- count/length(data_test[,1])
+}
+plot(bwvec, acc, xlab = "bandwidth", ylab = "accuracy", type = "l")
